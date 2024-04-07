ed3746c4a8b5527602f0a77c1ccd032883608b44
Beata Silwanowicz <beatasilwanowicz@beatas-mbp.home>
2024-04-07 22:33:17 +0200

    task done

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,14 @@
 const x = 5;
 let result = 0;
+
+for (let i=0; i<=x; i++) {
+    result = result +i
+    console.log(result)
+}
+
+let i = 0
+while (i <= x) {
+    i+=1
+    result = result +i
+    console.log(result)
+}
--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,11 @@
-const x = 10;
+const x = 4;
 let iteration = 0;
-let randomNumber = -1;
+let randomNumber
+
+while (randomNumber !== x) {
+    randomNumber = Math.round(Math.random() * 15)
+    iteration = iteration +1
+    console.log(iteration)
+
+}
+alert ('to ta sama liczba')
